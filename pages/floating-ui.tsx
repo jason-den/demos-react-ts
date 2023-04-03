f42289114128f54d0a869fa0d5ec9c8092cd27d4
Jason Den <jason.den@pwc.com>
2023-04-03 14:48:44 +1000

    wip

--- pages/floating-ui.tsx
+++ pages/floating-ui.tsx
@@ -1,20 +1,42 @@
-import { useFloating } from "@floating-ui/react";
+import { useEffect, useRef } from "react";
 
 export const FloatingUIExample = () => {
-	useFloating();
+	const floating = useRef<HTMLElement>();
+	const reference = useRef<HTMLElement>();
+
+	/*
+	TODO: use computePosition, autoUpdate and shift, to display the div id="floating" next to the div id="reference", whenever user scoll the sub-containers
+	insde id="container"
+	*/
+	useEffect(() => {
+		return () => {};
+	}, [floating, reference]);
+
 	return (
-		<div
-			className="tw-w-70 tw-h-80 tw-bg-grey-placeholder"
-			// style={}
-			// 	className="tw-w-70 tw-h-80  tw-rounded-sm tw-border-grey tw-border-solid tw-border-2
-			// tw-overflow-auto tw-flex tw-gap-3"
-		>
-			scroll container
-			{/* <div className="tw-w-50 tw-h-50 tw-mx-auto "> container 1 </div>
-			<div className="tw-w-50 tw-h-50 tw-mx-auto "> container 1 </div>
-			<div className="tw-w-50 tw-h-50 tw-mx-auto "> container 1 </div>
-			<div className="tw-w-50 tw-h-50 tw-mx-auto "> container 1 </div> */}
-			<p className="text-3xl tw-font-bold underline">Hello world!</p>
+		<div>
+			<div
+				id="container"
+				className="tw-w-[700px] tw-h-[500px]  tw-rounded-sm tw-border-grey tw-border-solid tw-border-2
+			tw-overflow-scroll "
+			>
+				scroll container
+				<div className="tw-w-50 tw-h-80 tw-bg-data-lighter-green tw-m-2">
+					sub-container 1{" "}
+					<div id="reference" className="tw-bg-grey-lightest tw-p-2 tw-m-2 tw-w-20 tw-rounded-md">
+						floating target
+					</div>
+				</div>
+				<div className="tw-w-50 tw-h-80 tw-bg-data-lighter-green tw-m-2"> sub-container 2 </div>
+				<div className="tw-w-50 tw-h-80 tw-bg-data-lighter-green tw-m-2"> sub-container 3 </div>
+				<div className="tw-w-50 tw-h-80 tw-bg-data-lighter-green tw-m-2"> sub-container 4 </div>
+			</div>
+			<div
+				id="floating"
+				// style={{ top: y ?? 0, left: x ?? 0, position: strategy }}
+				className="tw-w-50 tw-w-50 tw-bg-blue tw-opacity-50 tw-rounded-md"
+			>
+				floating
+			</div>
 		</div>
 	);
 };
